e5d845a6b73b5645a040d46e96eec060a1bbbc68
victorleite40 <victorsalvetti@hotmail.com>
2020-06-09 22:19:48 -0300

    Created server-side

---
+++ server/src/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+const routes = express.Router();
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+
+export default routes;
